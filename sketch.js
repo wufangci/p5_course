5332f06a5363a03b8e1ee951bd0e22e8efcc2839
wufangci <cicichen0825@gmail.com>
2021-12-25 13:29:47 +0800

    add : base map function example

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(100);
 }
 
 function draw() {
-  background(220);
+  background(100);
+
+  // 原始值 0 ~ width(畫布範圍) 目前的值 0 ~ 255 (顏色的範圍)
+  var colorR = map(mouseX, 0, windowWidth, 0, 255, true); // true 避開負值
+  textSize(40);
+  text(colorR, 50, 50);
+  // 填色依照範圍調整顏色範圍
+  fill(colorR);
+  ellipse(mouseX, mouseY, 50, 50);
 }
