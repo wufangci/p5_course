fa8d80051b2c9cdf77c91a861355337187f269ab
wufangci <cicichen0825@gmail.com>
2021-12-25 13:38:55 +0800

    update : random circle and changes color according to width

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100);
+  background(50);
 }
 
-function draw() {
-  background(100);
+var x, y;
 
-  // 原始值 0 ~ width(畫布範圍) 目前的值 0 ~ 255 (顏色的範圍)
-  var colorR = map(mouseX, 0, windowWidth, 0, 255, true); // true 避開負值
-  textSize(40);
-  text(colorR, 50, 50);
-  // 填色依照範圍調整顏色範圍
-  fill(colorR);
-  ellipse(mouseX, mouseY, 50, 50);
+function draw() {
+  // map() 原始值 0 ~ width(畫布範圍) 目前的值 0 ~ 255 (顏色的範圍)
+  x = random(windowWidth);
+  y = random(windowHeight);
+  var colorR = map(x, 0, windowWidth - 10, 0, 255, true); // 填色依照範圍調整顏色範圍
+  fill(colorR, 0, 0);
+  ellipse(x, y, 100, 100); // random 隨機改變 x,y 位置
 }
