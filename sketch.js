d77ff65d0e32a11666c3c7db2353b7ad518fd6e3
wufangci <cicichen0825@gmail.com>
2022-01-02 00:48:49 +0800

    add : L5 ifelse example

--- sketch.js
+++ sketch.js
@@ -4,33 +4,37 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  circleModel();
+  background(10);
+  drawRect();
 }
 
-function circleModel() {
-  // TODO 把指針概念加進去
-  for (let i = 0; i < 40; i++) {
-    for (let j = 0; j < 5; j++) {
-      const colorRange = map(i, 0, 10, 0, 255);
-      const frameCountRange = map(i, 0, frameCount, 0, i / 2, true);
-      const circleSize = (i * frameCount) / j + 1.5;
+function drawRect() {
+  // L5 example loop and if else
+  var count = 0; // counter 當累加的改變條件
+  for (var i = 0; i < windowWidth; i += 50) {
+    for (var j = 0; j < windowHeight; j += 50) {
+      // 間格 50 畫一個方形
 
-      console.log(i, "i");
-      console.log(j, "j");
-      console.log(circleSize, "circleSize");
-      console.log(colorRange, "colorRange");
-      console.log(frameCountRange, "frameCountRange");
+      if (count % 5 < 4) {
+        fill(random(200, 255), random(255), random(255), 200); // 隨機方形取顏色
+        stroke(random(200, 255), random(255), random(255), 200); // 隨機線條取顏色
 
-      noFill();
-      stroke(colorRange, 20);
-      strokeWeight(frameCountRange);
+        rect(i, j, 40, 40, 5);
+        if (random() < 0.5) {
+          fill(10);
+          ellipse(i + 20, j + 20, 20);
+          noStroke();
 
-      ellipse(windowWidth / 2, windowHeight / 2, circleSize);
-      if (j % 5 == 0) {
-        strokeWeight(map(i, 0, frameCount, 0, i));
-        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
+          if (random() < 0.5) {
+            noStroke();
+            fill(random(200, 255), random(255), random(255), 200); // 隨機方形取顏色
+            ellipse(i + 20, j + 20, 10);
+          }
+        }
       }
+      count += 1;
+      console.log(i, "i");
+      console.log(count, "count");
     }
   }
 }
