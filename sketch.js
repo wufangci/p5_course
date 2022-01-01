460c5efb167dc57bf58e3a5dad0faf17e009ea11
wufangci <cicichen0825@gmail.com>
2022-01-01 23:45:48 +0800

    add : circleModel

--- sketch.js
+++ sketch.js
@@ -6,36 +6,27 @@ function setup() {
 function draw() {
   background(0);
   circleModel();
-  // circleModel2();
 }
 
 function circleModel() {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 40; i++) {
     for (let j = 0; j < 5; j++) {
       const colorRange = map(i, 0, 10, 0, 255);
-      const frameCountRange = map(i, 0, frameCount, 0, i / 2);
+      const frameCountRange = map(i, 0, frameCount, 0, i / 2, true);
+      const circleSize = (i * frameCount) / j + 1.5;
+
+      console.log(i, "i");
+      console.log(j, "j");
+      console.log(circleSize, "circleSize");
+      console.log(colorRange, "colorRange");
+      console.log(frameCountRange, "frameCountRange");
 
       noFill();
       stroke(colorRange, 20);
       strokeWeight(frameCountRange);
 
-      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
-      if (j % 2 == 0) {
-        strokeWeight(map(i, 0, frameCount, 0, i));
-        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
-      }
-    }
-  }
-}
-
-function circleModel2() {
-  for (var i = 0; i < 50; i--) {
-    for (var j = 0; j < 5; j--) {
-      noFill();
-      stroke(map(i, 0, 10, 0, 255), 20);
-      strokeWeight(map(i, 0, frameCount, 0, i / 2));
-      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
-      if (j % 2 == 0) {
+      ellipse(windowWidth / 2, windowHeight / 2, circleSize);
+      if (j % 5 == 0) {
         strokeWeight(map(i, 0, frameCount, 0, i));
         ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
       }
