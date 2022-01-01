7be7be808a855e6747ae7a11dca7bd0df2bcb17b
wufangci <cicichen0825@gmail.com>
2022-01-01 21:26:57 +0800

    refactor：circle model

--- sketch.js
+++ sketch.js
@@ -1,30 +1,44 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = 500;
-  y = 500;
-  // delta = 10;
-  background(0);
+  background(10);
 }
 
-var x, y;
-var ang = 0;
-
 function draw() {
-  // background();
-  for (i = 500; i >= 2; i -= 10) {
-    var time = second();
-    var count = map(i, 0, frameCount, 0, i / 2, true);
-    var ang = time * i * -count;
+  background(0);
+  circleModel();
+  // circleModel2();
+}
 
-    strokeWeight(0.5);
-    var colorR = map(x, 0, i, 0, 10, true); // 填色依照範圍調整顏色範圍
-    stroke(random(100, 150));
+function circleModel() {
+  for (let i = 0; i < 50; i++) {
+    for (let j = 0; j < 5; j++) {
+      const colorRange = map(i, 0, 10, 0, 255);
+      const frameCountRange = map(i, 0, frameCount, 0, i / 2);
 
-    fill(10);
-    ellipse(width / 2, height / 2, i, i);
-    fill(100);
-    ellipse(width / 2, height / 2, i / 2, i / 2);
-    fill(colorR, 7);
-    arc(width / 2, height / 2, i + count, i + count, 0, ang, PI + QUARTER_PI);
+      noFill();
+      stroke(colorRange, 20);
+      strokeWeight(frameCountRange);
+
+      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
+      if (j % 2 == 0) {
+        strokeWeight(map(i, 0, frameCount, 0, i));
+        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
+      }
+    }
+  }
+}
+
+function circleModel2() {
+  for (var i = 0; i < 50; i--) {
+    for (var j = 0; j < 5; j--) {
+      noFill();
+      stroke(map(i, 0, 10, 0, 255), 20);
+      strokeWeight(map(i, 0, frameCount, 0, i / 2));
+      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
+      if (j % 2 == 0) {
+        strokeWeight(map(i, 0, frameCount, 0, i));
+        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
+      }
+    }
   }
 }
