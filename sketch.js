2fee64567e0686c8d0446a4ace2ca7c44a8c2431
wufangci <cicichen0825@gmail.com>
2022-01-02 12:27:48 +0800

    add : L6 RGB example

--- sketch.js
+++ sketch.js
@@ -1,44 +1,25 @@
+var clr;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(10);
+  background("#0000FF");
+  clr = color("#DC143C");
+  clr.setAlpha(100);
 }
 
 function draw() {
-  background(0);
-  circleModel();
-  // circleModel2();
+  /** L6
+   *  examples
+   *  _getBlue()  拿到 RGB 的 B 值
+   *  setBlue() 設定 RGB 的 B 值
+   **/
+
+  stroke("#0000FF");
+  strokeWeight(3);
+  fill(clr);
+  ellipse(mouseX, mouseY, 100);
 }
 
-function circleModel() {
-  for (let i = 0; i < 50; i++) {
-    for (let j = 0; j < 5; j++) {
-      const colorRange = map(i, 0, 10, 0, 255);
-      const frameCountRange = map(i, 0, frameCount, 0, i / 2);
-
-      noFill();
-      stroke(colorRange, 20);
-      strokeWeight(frameCountRange);
-
-      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
-      if (j % 2 == 0) {
-        strokeWeight(map(i, 0, frameCount, 0, i));
-        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
-      }
-    }
-  }
-}
-
-function circleModel2() {
-  for (var i = 0; i < 50; i--) {
-    for (var j = 0; j < 5; j--) {
-      noFill();
-      stroke(map(i, 0, 10, 0, 255), 20);
-      strokeWeight(map(i, 0, frameCount, 0, i / 2));
-      ellipse(windowWidth / 2, windowHeight / 2, (i * frameCount) / j + 1.5);
-      if (j % 2 == 0) {
-        strokeWeight(map(i, 0, frameCount, 0, i));
-        ellipse(windowWidth / 2, windowHeight / 2, i / frameCount);
-      }
-    }
-  }
+function mousePressed() {
+  var b = clr._getBlue();
+  clr.setBlue(b + 20); // 每按一次滑鼠就增加 B 的值
 }
